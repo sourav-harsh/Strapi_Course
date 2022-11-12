319a7f4f1de2b674d7270681d1e73f9bc63d07a1
Sourav Kumar <74768346+sourav-harsh@users.noreply.github.com>
2022-11-12 09:10:47 +0530

    fully functional app

--- client/Components/NavBar.js
+++ client/Components/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
-const NavBar = () => {
+
+const NavBar = ({cart}) => {
   return (
     <div>
     <header className="text-gray-400 bg-white body-font">
@@ -14,6 +15,7 @@ const NavBar = () => {
         <Link href="/about" legacyBehavior><a className="mr-5 hover:text-purple-400">About</a></Link>
         <Link href="/product" legacyBehavior><a className="mr-5 hover:text-purple-400">Product</a></Link>
         <Link href="/contact" legacyBehavior><a className="mr-5 hover:text-purple-400">Contact Us</a></Link>
+        <Link href="/checkout" legacyBehavior><a className="mr-5 hover:text-purple-400">Cart({cart.length})</a></Link>
       </nav>
       <button className ="my-2 text-white bg-indigo-500 border-0 py-0 md:py-2 px-3 md:px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Login</button>
     </div>
